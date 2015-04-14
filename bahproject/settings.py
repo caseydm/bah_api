e8937590334dc0a17cfc87a281e17ec2ba0788f7
Casey M <caseym@gmail.com>
2015-04-14 17:44:41 -0400

    static files

--- bahproject/settings.py
+++ bahproject/settings.py
@@ -13,11 +13,9 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '7&4+d-%yw(*fop@gijt*q5l0piduf7y#898egwu+n_^w#z%b*3'
@@ -26,7 +24,6 @@ SECRET_KEY = '7&4+d-%yw(*fop@gijt*q5l0piduf7y#898egwu+n_^w#z%b*3'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
