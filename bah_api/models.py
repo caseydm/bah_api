5d8e7749572dc805063688dad825549b02aad11a
Casey M <caseym@gmail.com>
2015-04-13 23:28:39 -0400

    added model

--- bah_api/models.py
+++ bah_api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+# BAH model
+class BAH(models.Model):
+	rank = models.CharField(max_length = 4)
+	dependents = models.BooleanField()
+	MHA = models.CharField(max_length = 5)
+
+		
--- bahproject/urls.py
+++ bahproject/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'bahproject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    # url(r'^bah_api/', include('bah_api.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('bah_api.urls')),
 ]
