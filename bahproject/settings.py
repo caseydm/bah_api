af158032e37dda3e1d34a2e43917732fa5f3f626
Casey M <caseym@gmail.com>
2015-04-14 17:51:51 -0400

    static files

--- bahproject/settings.py
+++ bahproject/settings.py
@@ -131,3 +131,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
 }
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
