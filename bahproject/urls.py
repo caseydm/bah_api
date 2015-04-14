b8bc0613ae09a7ea3baf334711304a9fdad1a396
Casey M <caseym@gmail.com>
2015-04-14 17:32:55 -0400

    Fixed errors

--- bahproject/urls.py
+++ bahproject/urls.py
@@ -4,23 +4,24 @@ from bah_api.models import BAH
 from rest_framework import routers, serializers, viewsets
 
 # Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class BAHSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = BAH
         fields = ('rank', 'dependents', 'MHA', 'rate')
 
 # ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+class BAHViewSet(viewsets.ModelViewSet):
+    queryset = BAH.objects.all()
+    serializer_class = BAHSerializer
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
+router.register(r'BAH', BAHViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
