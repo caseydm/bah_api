0fc16e666228010e257b45063f04416ebd400227
Casey M <caseym@gmail.com>
2015-04-15 06:48:32 -0400

    first commit on user_API

--- bahproject/urls.py
+++ bahproject/urls.py
@@ -1,27 +1,28 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from bah_api.models import BAH
-from rest_framework import routers, serializers, viewsets
+from bah_api import views
+#from bah_api.models import BAH
+#from rest_framework import routers, serializers, viewsets
 
-# Serializers define the API representation.
-class BAHSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = BAH
-        fields = ('rank', 'dependents', 'MHA', 'rate')
+# # Serializers define the API representation.
+# class BAHSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = BAH
+#         fields = ('rank', 'dependents', 'MHA', 'rate')
 
-# ViewSets define the view behavior.
-class BAHViewSet(viewsets.ModelViewSet):
-    queryset = BAH.objects.all()
-    serializer_class = BAHSerializer
+# # ViewSets define the view behavior.
+# class BAHViewSet(viewsets.ModelViewSet):
+#     queryset = BAH.objects.all()
+#     serializer_class = BAHSerializer
 
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'BAH', BAHViewSet)
+# # Routers provide an easy way of automatically determining the URL conf.
+# router = routers.DefaultRouter()
+# router.register(r'BAH', BAHViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^', include(router.urls)),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^user/', views.UserList.as_view()),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
 ]
