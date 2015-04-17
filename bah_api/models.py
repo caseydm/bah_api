8ec79a1b2849b7c281aefa8cb6706a752960c81f
Casey M <caseym@gmail.com>
2015-04-17 19:57:09 -0400

    removed old BAH model

--- bah_api/models.py
+++ bah_api/models.py
@@ -1,8 +1,32 @@
 from django.db import models
 
-# BAH model
-class BAH(models.Model):
-	rank = models.CharField(max_length = 4)
-	dependents = models.BooleanField()
-	MHA = models.CharField(max_length = 5)
-	rate = models.DecimalField(max_digits=15, decimal_places=2)
+class withDependents(models.Model):
+	MHA = model.CharField(max_length = 5, primary_key = True)
+	E1 = models.DecimalField(max_digits = 7, decimal_places = 2)
+	E2 = models.DecimalField(max_digits = 7, decimal_places = 2)
+	E3 = models.DecimalField(max_digits = 7, decimal_places = 2)
+	E4 = models.DecimalField(max_digits = 7, decimal_places = 2)
+	E5 = models.DecimalField(max_digits = 7, decimal_places = 2)
+	E6 = models.DecimalField(max_digits = 7, decimal_places = 2)
+	E7 = models.DecimalField(max_digits = 7, decimal_places = 2)
+	E8 = models.DecimalField(max_digits = 7, decimal_places = 2)
+	E9 = models.DecimalField(max_digits = 7, decimal_places = 2)
+	W1 = models.DecimalField(max_digits = 7, decimal_places = 2)
+	W2 = models.DecimalField(max_digits = 7, decimal_places = 2)
+	W3 = models.DecimalField(max_digits = 7, decimal_places = 2)
+	W4 = models.DecimalField(max_digits = 7, decimal_places = 2)
+	W5 = models.DecimalField(max_digits = 7, decimal_places = 2)
+	O1E = models.DecimalField(max_digits = 7, decimal_places = 2)
+	O2E = models.DecimalField(max_digits = 7, decimal_places = 2)
+	O3E = models.DecimalField(max_digits = 7, decimal_places = 2)
+	O1 = models.DecimalField(max_digits = 7, decimal_places = 2)
+	O2 = models.DecimalField(max_digits = 7, decimal_places = 2)
+	O3 = models.DecimalField(max_digits = 7, decimal_places = 2)
+	O4 = models.DecimalField(max_digits = 7, decimal_places = 2)
+	O5 = models.DecimalField(max_digits = 7, decimal_places = 2)
+	O6 = models.DecimalField(max_digits = 7, decimal_places = 2)
+	O7 = models.DecimalField(max_digits = 7, decimal_places = 2)
+
+class ZipMHA(models.Model):
+	ZipCode = models.IntegerField(max_digits = 5)
+	MHA = model.CharField(max_length = 5, primary_key = True)
