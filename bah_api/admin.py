e97948ffa4fb52ce7fd2958a7691c262aa68913e
Casey M <caseym@gmail.com>
2015-04-19 15:29:31 -0400

    updated admin.py

--- bah_api/admin.py
+++ bah_api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import withDependents, ZipMHA
+from .models import withDependents, withOutDependents, ZipMHA
 
 # Register your models here.
 admin.site.register(withDependents)
+admin.site.register(withOutDependents)
 admin.site.register(ZipMHA)
