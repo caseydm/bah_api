83adc8793b03962de92b5ecebace02357d99f0ba
Casey M <caseym@gmail.com>
2015-04-19 07:20:51 -0400

    refactored import.py script

--- import.py
+++ import.py
@@ -1,38 +1,38 @@
 import csv
-from bah_api.models import withDependents, ZipMHA
+from bah_api.models import withDependents, withOutDependents, ZipMHA
 
-# with Dependents
-def LoadWithDep(file_location):
+# Populate CSV file into model
+def LoadCSV(file_location, my_model, delim):
 	f = open(file_location)
-	csv_f = csv.reader(f)
+	csv_f = csv.reader(f, delimiter=delim)
 	for row in csv_f:
 		i = 1
 		# create a model instance	
-		wdModel = withDependents()
+		target_model = my_model
 		#loop through the rows
 		for y in row:
-			setattr(wdModel, wdModel._meta.fields[i].name, y)
+			setattr(target_model, target_model._meta.fields[i].name, y)
 			i += 1
 		# save each row
-		wdModel.save()
+		target_model.save()
 	f.close()
 
-# ZipMHA
-def LoadZipMHA(file_location):
-	f = open(file_location)
-	csv_f = csv.reader(f, delimiter=' ')
-	for row in csv_f:
-		i = 1
-		# create a model instance	
-		zipModel = ZipMHA()
-		#loop through the rows
-		for y in row:
-			setattr(zipModel, zipModel._meta.fields[i].name, y)
-			i += 1
-		# save each row
-		zipModel.save()
-	f.close()
+# # ZipMHA
+# def LoadZipMHA(file_location):
+# 	f = open(file_location)
+# 	csv_f = csv.reader(f, delimiter=' ')
+# 	for row in csv_f:
+# 		i = 1
+# 		# create a model instance	
+# 		zipModel = ZipMHA()
+# 		#loop through the rows
+# 		for y in row:
+# 			setattr(zipModel, zipModel._meta.fields[i].name, y)
+# 			i += 1
+# 		# save each row
+# 		zipModel.save()
+# 	f.close()
 
 # run functions
-# LoadWithDep("BAH2015/bahw15.txt")
-LoadZipMHA("BAH2015/sorted_zipmha15.txt")
+# LoadCSV("BAH2015/bahw15.txt", withOutDependents(), ",")
+# LoadCSV("BAH2015/sorted_zipmha15.txt", ZipMHA(), " ")
