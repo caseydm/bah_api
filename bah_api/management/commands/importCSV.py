32f0720c7b0620fe3cf0eb23d08018d138cc3c2e
Casey M <caseym@gmail.com>
2015-04-20 21:16:36 -0400

    finished management command file

--- bah_api/management/commands/importCSV.py
+++ bah_api/management/commands/importCSV.py
@@ -1,24 +1,28 @@
+from django.core.management.base import BaseCommand
+from django.conf import settings
 import csv
-from settings import BASE_DIR
 from bah_api.models import withDependents, withOutDependents, ZipMHA
 
-# Populate CSV file into model
-def LoadCSV(file_location, my_model, delim):
-	f = open(file_location)
-	csv_f = csv.reader(f, delimiter=delim)
-	for row in csv_f:
-		i = 1
-		# create a model instance	
-		target_model = my_model()
-		#loop through the rows
-		for y in row:
-			setattr(target_model, target_model._meta.fields[i].name, y)
-			i += 1
-		# save each row
-		target_model.save()
-	f.close()
 
-# run functions
-LoadCSV(BASE_DIR + "BAH2015/bahw15.txt", withDependents, ',')
-LoadCSV(BASE_DIR + "BAH2015/bahwo15.txt", withOutDependents, ',')
-LoadCSV(BASE_DIR + "BAH2015/sorted_zipmha15.txt", ZipMHA, ' ')
+class Command(BaseCommand):
+	def handle(self, *args, **options):
+		# Populate CSV file into model
+		def LoadCSV(file_location, my_model, delim):
+			f = open(file_location)
+			csv_f = csv.reader(f, delimiter=delim)
+			for row in csv_f:
+				i = 1
+				# create a model instance	
+				target_model = my_model()
+				#loop through the rows
+				for y in row:
+					setattr(target_model, target_model._meta.fields[i].name, y)
+					i += 1
+				# save each row
+				target_model.save()
+			f.close()
+
+		# run functions
+		# LoadCSV(settings.PROJECT_PATH + "/BAH2015/bahw15.txt", withDependents, ',')
+		# LoadCSV(settings.PROJECT_PATH + "/BAH2015/bahwo15.txt", withOutDependents, ',')
+		# LoadCSV(settings.PROJECT_PATH + "/BAH2015/sorted_zipmha15.txt", ZipMHA, ' ')
