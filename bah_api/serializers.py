e2f9c848015ec18bdcd5a8d478c0a4af2f5258b2
Casey M <caseym@gmail.com>
2015-04-25 20:06:26 -0400

    edited views

--- bah_api/serializers.py
+++ bah_api/serializers.py
@@ -9,3 +9,12 @@ class withDependentsSerializer(serializers.ModelSerializer):
 		'W1', 'W2', 'W3', 'W4', 'W5', 'O1E', 'O2E', 'O3E', 'O1',
 		'O2', 'O3', 'O4', 'O5', 'O6', 'O7', 'O8', 'O9', 'O10'
 		]
+
+class withOutDependentsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = withOutDependents
+        fields = [
+		'E1', 'E2','E3', 'E4', 'E5', 'E6', 'E7', 'E8', 'E9',
+		'W1', 'W2', 'W3', 'W4', 'W5', 'O1E', 'O2E', 'O3E', 'O1',
+		'O2', 'O3', 'O4', 'O5', 'O6', 'O7', 'O8', 'O9', 'O10'
+		]
--- bah_api/views.py
+++ bah_api/views.py
@@ -5,22 +5,6 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-
-# get BAH rates based on MHA
-@api_view(['GET'])
-def rate_detail(request, MHA):
-    """
-    Retrieve BAH rates
-    """
-    try:
-        rates = withDependents.objects.get(MHA=MHA)
-    except withDependents.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = withDependentsSerializer(rates)
-        return Response(serializer.data)
-
 @api_view(['GET'])
 def zip_rates(request, Zip):
     """
