39edf96167a90882d6aa77a0f0ed1bb47b469d24
Casey M <caseym@gmail.com>
2015-08-03 22:32:59 -0400

    updated model

--- bah_api/models.py
+++ bah_api/models.py
@@ -1,31 +1,36 @@
 from django.db import models
 
+
 # withDependents table
 class withDependents(models.Model):
-	MHA = models.CharField(max_length = 5)
+    MHA = models.CharField(max_length=5)
 
-	def __str__(self):
-		return self.MHA
+    def __str__(self):
+        return self.MHA
+
 
 # withOutDependents table
 class withOutDependents(models.Model):
-	MHA = models.CharField(max_length = 5)
-	
-	def __str__(self):
-		return self.MHA
+    MHA = models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.MHA
+
 
 # Zip Code and MHA table
 class ZipMHA(models.Model):
-	ZipCode = models.CharField(max_length = 5)
-	MHA = models.CharField(max_length = 5)
+    ZipCode = models.CharField(max_length=5)
+    MHA = models.CharField(max_length=5)
 
 # add rank fields to withDependents and withOutDependents
 rank_fields = [
-	'E1', 'E2','E3', 'E4', 'E5', 'E6', 'E7', 'E8', 'E9',
-	'W1', 'W2', 'W3', 'W4', 'W5', 'O1E', 'O2E', 'O3E', 'O1',
-	'O2', 'O3', 'O4', 'O5', 'O6', 'O7', 'O8', 'O9', 'O10'
-	]
+    'E1', 'E2', 'E3', 'E4', 'E5', 'E6', 'E7', 'E8', 'E9',
+    'W1', 'W2', 'W3', 'W4', 'W5', 'O1E', 'O2E', 'O3E', 'O1',
+    'O2', 'O3', 'O4', 'O5', 'O6', 'O7', 'O8', 'O9', 'O10'
+]
 
 for rank in rank_fields:
-		withDependents.add_to_class(rank, models.DecimalField(max_digits = 10, decimal_places = 2))
-		withOutDependents.add_to_class(rank, models.DecimalField(max_digits = 10, decimal_places = 2))
+    withDependents.add_to_class(
+        rank, models.DecimalField(max_digits=10, decimal_places=2))
+    withOutDependents.add_to_class(
+        rank, models.DecimalField(max_digits=10, decimal_places=2))
