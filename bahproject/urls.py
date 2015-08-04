d79b6b24f1cd532a4265808b4b7684d07072b0a3
Casey M <caseym@gmail.com>
2015-08-03 21:59:11 -0400

    various bug fixed

--- bahproject/urls.py
+++ bahproject/urls.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 from bah_api import views
 
 urlpatterns = [
-	url(r'^$', views.index, name = 'index'),
+    url(r'^$', views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^(?P<zip_code>([0-9]{5}))/(?P<with_or_without>(\w+))/$', views.rates),
+    url(r'^(?P<zip_code>([0-9]{5}))/(?P<with_or_without>(\w+))/$',
+        views.rates),
 ]
