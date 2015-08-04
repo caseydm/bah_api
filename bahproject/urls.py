7aa7e2b5198725918363f91511a857eda3807739
Casey M <caseym@gmail.com>
2015-08-03 22:16:02 -0400

    updated URLs

--- bahproject/urls.py
+++ bahproject/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 from bah_api import views
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name="index.html")),
+    url(r'^$', TemplateView.as_view(template_name="bah_api/index.html")),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^(?P<zip_code>([0-9]{5}))/(?P<with_or_without>(\w+))/$',
         views.rates),
